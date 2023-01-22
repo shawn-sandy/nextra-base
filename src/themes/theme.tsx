0cf142e9b490b453e0ac82fbdb5da543d24ff289
shawn <shawnsandy04@gmail.com>
2023-01-22 11:41:08 -0500

    feat: ✨  add path mapping to imports

--- src/themes/theme.tsx
+++ src/themes/theme.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Nav, { NavItem } from "../components/nav";
+import Nav, { NavItem } from "@/components/nav";
 import { Box, Button, Aside, Header, Main, Footer, Section, Article, Title } from "@fpkit/react";
 
 import "@shawnsandy/first-paint/dist/css/libs/all.min.css";
