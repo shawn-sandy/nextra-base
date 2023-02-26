ef9a44bb3c3b8351a5e3cba1a1d880dbecdc2f32
shawn <shawnsandy04@gmail.com>
2023-02-26 06:28:19 -0500

    build: 📦 next/nextra, first-paint updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,16 @@
 // import { DocsThemeConfig } from 'nextra-theme-docs'
-
 export default {
+
   logo: <span>Nextra Base</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/shuding/nextra-docs-template'
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com'
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
-  },
+  }
+
 }
