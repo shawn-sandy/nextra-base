c0828c6070135748118e3ea9a0c67242310864e1
shawn <shawnsandy04@gmail.com>
2023-04-01 10:02:59 -0400

    chore: app updates
    - add .npmrc with peer dependency resolution
    - update pnpm-lock.yaml
    - update package.json
    - update theme.tsx
    - update index.mdx add type page frontmatter∑

--- src/themes/theme.tsx
+++ src/themes/theme.tsx
@@ -2,73 +2,49 @@ import Link from "next/link";
 import Nav, { NavList } from "@/components/nav";
 import ThemeHeader from "@/components/theme-header";
 import ArticleList from "@/components/articles-list";
-import { usePageOpts } from "@/components/usePageOpts";
-import {
-  Box,
-  Aside,
-  Main,
-  Footer,
-  Section,
-  Article
-} from "@fpkit/react";
+// import { usePageOpts } from "@/components/usePageOpts";
+import { Navigation, PostsList, ThemeMain } from "@fpkit/nextjs";
+import { usePageOpts } from "@fpkit/nextjs/libs/hooks";
+import { FilterMdxPages } from "@fpkit/nextjs/libs/libs";
+
+import { Box, Aside, Main, Footer, Section, Article } from "@fpkit/react";
 
 import "@shawnsandy/first-paint/dist/css/libs/all.min.css";
 
 import type { NextraThemeLayoutProps } from "nextra";
+import { log } from "console";
+import { MDXProvider } from "nextra/mdx";
 
 export default function Layout({
   children,
   pageOpts,
-  themeConfig
+  themeConfig,
 }: NextraThemeLayoutProps) {
+  const { pageMap } = pageOpts;
 
-  const { dirList, posts, postList} = usePageOpts({pageOpts});
-  
-  postList.map((item) => {
-    const { route, frontMatter } = item;
-  });
-  
+  const { dirList, postList } = usePageOpts({ options: { pageOpts } });
+
+  const latest = FilterMdxPages(pageMap);
+
+  const brand = { url: "/", logo: "Nextra Base" };
+
   return (
     <>
-      <Nav>
-        <ul>
-          <li>
-            <Box as="h1" styles={{ "--fs": "1.5rem" }}>
-              <Link href="/">{themeConfig.logo}</Link>
-            </Box>
-          </li>
-        </ul>
-        <ul>
-          <NavList items={dirList} />  
-        </ul>
-      </Nav>
+      <Navigation data={pageMap} brand={brand} />
       <ThemeHeader title={themeConfig.logo} />
-      <Main>
-        <Section data-content>
-          <Article>
-            {pageOpts.route === "/" ? (
-              <>
-                <ArticleList postList={postList}
-                showDescription />
-              </>
-            ) : (
-              children
-            )}
-          </Article>
-          <Aside>
-            <ul data-fp-list="unstyled">
-              <ArticleList
-                postList={postList} 
-                as="li"
-                elm="h4"
-              />
-            </ul>
-          </Aside>
-        </Section>
-      </Main>
-    
+      <ThemeMain>
+        {pageOpts.route === "/" && latest.length > 0 ? (
+          <PostsList postList={latest} showDescription />
+        ) : (
+          <MDXProvider components={themeConfig.components}>
+            {children}
+          </MDXProvider>
+        )}
+      </ThemeMain>
+
       <Footer>
         <p>&copy; {new Date().getFullYear()}</p>
       </Footer>
     </>
-  )};
+  );
+}
