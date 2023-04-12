06e7d65bcdf4ca3943af5fdf6acd744581f5dc3e
shawn <shawnsandy04@gmail.com>
2023-04-12 09:19:40 -0400

    feat; 🤩 update theme/packages
    - change the NavList prop postList to pages

--- src/themes/theme.tsx
+++ src/themes/theme.tsx
@@ -1,19 +1,9 @@
-import Link from "next/link";
 import Nav from "@/components/nav";
 import ThemeHeader from "@/components/theme-header";
-import ArticleList from "@/components/articles-list";
-// import { usePageOpts } from "@/components/usePageOpts";
-import {
-  NavBrand,
-  Navigation,
-  PostsList,
-  ThemeMain,
-  NavList,
-} from "@fpkit/nextjs";
-import { usePageOpts } from "@fpkit/nextjs/libs/hooks";
+import { NavBrand, PostsList, ThemeMain, NavList } from "@fpkit/nextjs";
 import { FilterMdxPages, FilterPageType } from "@fpkit/nextjs/libs/libs";
 
-import { Box, Aside, Main, Footer, Section, Article, Tag } from "@fpkit/react";
+import { Footer } from "@fpkit/react";
 
 import "@shawnsandy/first-paint/dist/css/libs/all.min.css";
 
@@ -31,15 +21,13 @@ export default function Layout({
   const latest = FilterMdxPages(pageMap);
   const pages = FilterPageType(pageMap);
 
-  console.log({ pages });
-
   const brand = { url: "/", logo: "Nextra Base" };
 
   return (
     <>
       <Nav>
         <NavBrand logo={themeConfig.brand.logo} url={themeConfig.brand.url} />
-        <NavList postList={pages} />
+        <NavList pages={pages} />
       </Nav>
       <ThemeHeader title={themeConfig.logo} />
       <ThemeMain>
