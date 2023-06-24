6a261e0453fe31b23e084387083f75d8872e27a9
shawn <shawnsandy04@gmail.com>
2023-06-24 18:18:01 -0400

    docs(theme.tsx): add JSDoc comments to the Layout, SideBar, and ThemeHeader components to improve code documentation
    chore(package.json): update "@shawnsandy/first-paint" dependency from version "^2.17.56" to "^2.17.59"
    feat(theme.tsx): add support for rendering latest posts on the home page if there are any, otherwise render child components

--- src/themes/theme.tsx
+++ src/themes/theme.tsx
@@ -11,6 +11,13 @@ import type { MdxFile, NextraThemeLayoutProps } from "nextra";
 import { MDXProvider } from "nextra/mdx";
 import { log } from "console";
 
+/**
+ * The main layout component for the Nextra theme.
+ * @param children The child components to render.
+ * @param pageOpts The page options for the current page.
+ * @param themeConfig The configuration options for the theme.
+ * @returns The rendered layout component.
+ */
 export default function Layout({
   children,
   pageOpts,
@@ -23,6 +30,10 @@ export default function Layout({
 
   const brand = { url: "/", logo: "Nextra Base" };
 
+  /**
+   * The sidebar component for the layout.
+   * @returns The rendered sidebar component.
+   */
   const SideBar = () => {
     return (
       <>
@@ -36,12 +47,18 @@ export default function Layout({
 
   return (
     <>
+      {/* The navigation component */}
       <Nav>
         <NavBrand logo={themeConfig.brand.logo} url={themeConfig.brand.url} />
         <NavList pages={pages} />
       </Nav>
+
+      {/* The header component */}
       <ThemeHeader title={themeConfig.logo} />
+
+      {/* The main content component */}
       <ThemeMain sideBar={<SideBar />}>
+        {/* Render the latest posts or the child components */}
         {pageOpts.route === "/" && latest.length > 0 ? (
           <PostsList postList={latest} showDescription />
         ) : (
@@ -51,6 +68,7 @@ export default function Layout({
         )}
       </ThemeMain>
 
+      {/* The footer component */}
       <Footer>
         <section>
           <p>&copy; {new Date().getFullYear()}</p>
